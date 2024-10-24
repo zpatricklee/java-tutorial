98cd06a1f4f64f894ae1f9f100b0707ccfad99d7
zpatricklee <zpatricklee@gmail.com>
2024-10-24 00:04:15 -0700

    replaced XML in web.xml with Annotations i.e. @WebServlet("/add")

--- src/main/java/com/plee/AddServlet.java
+++ src/main/java/com/plee/AddServlet.java
@@ -1,17 +1,15 @@
 package com.plee;
 
 import java.io.IOException;
-import java.io.PrintWriter;
 
-import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 
-
+@WebServlet("/add")
 public class AddServlet extends HttpServlet{
 	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException, ServletException {
 		int i = Integer.parseInt(req.getParameter("num1"));
--- src/main/java/com/plee/SquareServlet.java
+++ src/main/java/com/plee/SquareServlet.java
@@ -3,12 +3,13 @@ package com.plee;
 import java.io.IOException;
 import java.io.PrintWriter;
 
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 
+@WebServlet("/square")
 public class SquareServlet extends HttpServlet {
 	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
 		int k = 0;
